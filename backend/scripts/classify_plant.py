f6800c5774f131b1ee203a479c7077ed1060cbcd
shauryam-singh <shauryamsingh9@gmail.com>
2025-08-17 23:47:56 +0530

    reducing memory for render

--- backend/scripts/classify_plant.py
+++ backend/scripts/classify_plant.py
@@ -1,9 +1,9 @@
+# scripts/classify_plant.py
 import cv2
 import numpy as np
 import joblib
 from skimage.feature import hog
 from skimage import color
-import io
 
 plant_classifier = joblib.load('models/plant_model_compressed.pkl')
 
@@ -11,12 +11,15 @@ def extract_features_from_bytes(file_bytes):
     image = cv2.imdecode(np.frombuffer(file_bytes, np.uint8), cv2.IMREAD_COLOR)
     if image is None:
         raise ValueError("Cannot read image bytes")
+    
     image = cv2.resize(image, (128, 128))
     gray = color.rgb2gray(image)
-    hog_features, _ = hog(
-        gray, visualize=True, block_norm='L2-Hys',
+
+    hog_features = hog(
+        gray, visualize=False, block_norm='L2-Hys',
         pixels_per_cell=(16, 16), cells_per_block=(2, 2)
     )
+
     hist_features = cv2.calcHist(
         [image], [0, 1, 2], None,
         [8, 8, 8], [0, 256, 0, 256, 0, 256]
